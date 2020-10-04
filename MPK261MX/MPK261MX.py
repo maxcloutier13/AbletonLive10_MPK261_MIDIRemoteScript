c6cc0f95d57093846bbef867a61da5518228df3a
Maxime Cloutier <56617439+maxcloutier13@users.noreply.github.com>
2020-10-04 14:12:13 -0400

    Update MPK261MX.py
    Cleaned up a bit the needless verticality of the original file

--- MPK261MX/MPK261MX.py
+++ MPK261MX/MPK261MX.py
@@ -11,7 +11,7 @@ from _Framework.MidiMap import MidiMap as MidiMapBase
 from _Framework.MidiMap import make_button, make_encoder, make_slider
 from _Framework.InputControlElement import MIDI_NOTE_TYPE, MIDI_CC_TYPE
 
-#MAX - Custom make_encoder function to use RELATIVE knobs instead of the default absolute
+#Custom make_encoder function to use RELATIVE knobs instead of the default absolute
 def make_encoder(name, channel, number, midi_message_type):
  return EncoderElement(midi_message_type, channel, number, Live.MidiMap.MapMode.relative_two_compliment, name=name)
 
@@ -25,47 +25,10 @@ class MidiMap(MidiMapBase):
         self.add_button('Loop', 0, 114, MIDI_CC_TYPE)
         self.add_button('Forward', 0, 115, MIDI_CC_TYPE)
         self.add_button('Backward', 0, 116, MIDI_CC_TYPE)
-        self.add_matrix('Sliders', make_slider, 0, [[12,
-          13,
-          14,
-          15,
-          16,
-          17,
-          18,
-          19]], MIDI_CC_TYPE)
-        self.add_matrix('Encoders', make_encoder, 0, [[22,
-          23,
-          24,
-          25,
-          26,
-          27,
-          28,
-          29]], MIDI_CC_TYPE)
-        self.add_matrix('Arm_Buttons', make_button, 0, [[32,
-          33,
-          34,
-          35,
-          36,
-          37,
-          38,
-          39]], MIDI_CC_TYPE)
-        self.add_matrix('Drum_Pads', make_button, 1, [[81,
-          83,
-          84,
-          86],
-         [74,
-          76,
-          77,
-          79],
-         [67,
-          69,
-          71,
-          72],
-         [60,
-          62,
-          64,
-          65]], MIDI_NOTE_TYPE)
-
+        self.add_matrix('Sliders', make_slider, 0, [[12, 13, 14, 15, 16, 17, 18, 19]], MIDI_CC_TYPE)
+        self.add_matrix('Arm_Buttons', make_button, 0, [[32, 33, 34, 35, 36, 37, 38, 39]], MIDI_CC_TYPE)
+        self.add_matrix('Encoders', make_encoder, 0, [[22, 23, 24, 25, 26,  27, 28, 29]], MIDI_CC_TYPE)
+        self.add_matrix('Drum_Pads', make_button, 1, [[81, 83, 84, 86],[74, 76, 77, 79], [67, 69, 71, 72],[60, 62, 64, 65]], MIDI_NOTE_TYPE)
 
 class MPK261MX(ControlSurface):
 
@@ -81,6 +44,7 @@ class MPK261MX(ControlSurface):
             device = DeviceComponent(name='Device', is_enabled=False, layer=Layer(parameter_controls=midimap['Encoders']))
             device.set_enabled(True)
             self.set_device_component(device)
+            #Mixer0 1-8 on channel 1
             mixer_size = len(midimap['Sliders'])
             mixer = MixerComponent(mixer_size, name='Mixer', is_enabled=False, layer=Layer(volume_controls=midimap['Sliders'], arm_buttons=midimap['Arm_Buttons']))
             mixer.set_enabled(True)
